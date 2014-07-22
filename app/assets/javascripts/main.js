c1924e0b72b8abd26f43ce508bb21e2b83f7141a
Ben White <whtouche@gmail.com>
2014-07-22 16:59:42 -0400

    Added comments

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
       articlesHTML += '<li id=article_' + articles[i].id + '>' + articles[i].title;
       articlesHTML += '<div>' + articles[i].body + '</div>';
       articlesHTML += '</li>';
-    };
+    }
 
     // Clear the list of articles
     $('#articles').empty();
@@ -36,37 +36,37 @@ $(document).ready(function(){
     return html;
   },
   getArticle = function(article){
-    // Add the article to the article list                                   
+    // Add the article to the article list
     $('#articles').append(articleHTML(article));
   },
   createArticleCallbackHandler = function(event){
-    // New article form                                                      
+    // New article form
     var $form = $(event.target),
     $title = $form.find("input[name='title']"),
     $body = $form.find("input[name='body']"),
-    // Get the form action                                                   
+    // Get the form action
     action = $form.attr('action'),
     requestObj = {article:  {title: $title.val(), body: $body.val()}};
 
     event.preventDefault();
-      
-    // Create and send a POST request                                        
+
+    // Create and send a POST request
     $.ajax({
       type: "POST",
-      url: 'http://localhost:3000/articles', 
+      url: 'http://localhost:3000/articles',
       data: requestObj,
       dataType: 'json'
     }).done(getArticle);
 
     // Clear the form
     $title.val("");
-    $body.val("");    
+    $body.val("");
   };
 
   // Set up click handler for getting articles.
   $('#get-articles').on('click', getArticles);
 
-  // Set up click handler for form submit                                    
+  // Set up click handler for form submit
   $('#new-article').on('submit', createArticleCallbackHandler);
 
   // Simulate a user click event.
